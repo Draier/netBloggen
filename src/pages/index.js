c7a682aae9eb2dbb25ad1d5b138cd3eaa691cd4a
Luis Balbás <lbq200@gmail.com>
2018-06-26 16:03:47 -0400

    Adding blog post pagination to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,41 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+const IndexPage = ({data}) => {
+	console.log(data);
+	return (
+		<div>
+			<h1>Welcome to this blog</h1>
+			<div>
+				{data.allMarkdownRemark.edges.map(post => (
+					<Link to={post.node.fields.slug}>
+						<div>
+							<h2>{post.node.frontmatter.title}</h2>
+							<p>{post.node.frontmatter.description}</p>
+						</div>
+					</Link>
+					))}
+			</div>
+		</div>
+	)
+}
+
+export const PaginatorQuery = graphql`
+	query IndexQuery {
+	  allMarkdownRemark(limit: 10){
+	    edges{
+	      node {
+	        fields {
+	          slug
+	        }
+	        frontmatter{
+	          title
+	          description
+	        }
+	      }
+	    }
+	  }
+	}
+`
+
+export default IndexPage;
--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -6,6 +6,13 @@ export default function Template({data}) {
 
 	return (
 		<div>
+			<Helmet
+        title={"netBloggen | " + post.frontmatter.title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
 			<h1>{post.frontmatter.title}</h1>
 			<p>{post.frontmatter.description}</p>
 			<p>Published {post.frontmatter.date}</p>
