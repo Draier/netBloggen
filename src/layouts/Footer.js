3ec3122c3b69c655610a9715918977dd6805c90e
Luis Balbás <lbq200@gmail.com>
2018-08-13 15:31:29 -0400

    social media links and icons

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Helmet from 'react-helmet'
 import Link from 'gatsby-link';
-
+import icon from './icon.png';
+import {IoLogoTwitter, IoLogoGithub, IoMdDesktop} from 'react-icons/io'
 
 const Footer = () => (
 <div
@@ -13,14 +15,18 @@ const Footer = () => (
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        paddingTop : '0.7rem',
+        paddingTop : '0.8rem',
         padding: '0.7rem 0 1rem 0',
       }}
     >
-    <div className="search">
-        
-    </div> 
-   <h1 style={{ 
+    <nav className="search">
+        <ul className="social-container">
+          <li><a className="io-cont" href="https://twitter.com/draier_wdv"><IoLogoTwitter/></a></li>
+          <li><a className="io-cont" href="https://github.com/Draier/"><IoLogoGithub/></a></li>
+          <li><a className="io-cont" href="https://draier.me"><IoMdDesktop/></a></li>
+        </ul>
+    </nav> 
+   <h1 className="brand" style={{ 
         margin: 0 }}>
         <Link
           to="/"
@@ -33,6 +39,22 @@ const Footer = () => (
         </Link>
       </h1>
     </div>
+    <Helmet
+        title="netBloggen | Home"
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+        link={[{
+          rel: 'shortcut icon',
+          type: 'image/x-icon',
+          href: `${icon}`
+        }]}
+        script={[
+          {
+            src: 'https://unpkg.com/ionicons@4.3.0/dist/ionicons.js'
+          }]}
+      />
 </div>
 
 )
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -17,7 +17,7 @@ const Header = () => (
         padding: '1.45rem 0',
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 className="brand" style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
